08837eae076232870c0d015ac0b404579db2f041
lesves <lukas.veskrna@gmail.com>
2018-02-22 16:46:35 +0100

    Added base structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// Tady jsem jenom přepsal tu strukturu, co jsme navrhli
+
+
+enum Content {
+    Text(String),
+    Equation(String), // Zatím string
+    File(String, Vec<u8>),
+    HTML(String),
+}
+
+enum Target {
+    User(String),
+    Channel(String),
+    Houbi,
+}
+
+enum Packet {
+    Message {
+        sender: String,
+        hash: String,
+        content: Content,
+        target: Target,
+    },
+}
+
 fn main() {
     println!("Hello, world!");
 }
